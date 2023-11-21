11c8ae375e980ce5e50c9c10ff6e434257fcd1bd
Rima <rimameih@gmail.com>
2023-11-21 22:36:20 +0700

    refactor: folder

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,6 +1,6 @@
 const hexToBinary = require('hex-to-binary');
 const Block = require('./block');
-const { cryptoHash } = require('../crypto-hash');
+const { cryptoHash } = require('../utils/crypto-hash');
 const { GENESIS_DATA, MINE_RATE } = require('../config');
 
 describe('test block', () => {
